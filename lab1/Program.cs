b0e40ae1dd8681d8dfbd80f2d6f1497ca4909fd2
vakuliukpipz23-glitch <vakuliuk.p_ipz23@rcit.ukr.education>
2025-09-05 23:06:57 +0300

    my first oop

---
+++ lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lab1
+{
+    public class Figure
+    {
+        
+        private double area;
+
+       
+        public double Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
+
+        
+        public Figure(double area)
+        {
+            this.area = area;
+            Console.WriteLine("Конструктор Figure викликаний");
+        }
+
+
+        ~Figure()
+        {
+            Console.WriteLine("Деструктор Figure викликаний");
+        }
+
+
+        public string GetFigure()
+        {
+            return $"Фігура з площею {area}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Figure f = new Figure(25.5);
+
+            Console.WriteLine(f.GetFigure());
+
+            f.Area = 40;
+            Console.WriteLine(f.GetFigure());
+        }
+    }
+}
