bf49971336b5e4c9c0a6964a32bec38406932687
vakuliukpipz23-glitch <vakuliuk.p_ipz23@rcit.ukr.education>
2026-02-04 16:17:45 +0200

    lab22_v1

--- lab22_v1/obj/Debug/net10.0/lab22_v1.AssemblyInfo.cs
+++ lab22_v1/obj/Debug/net10.0/lab22_v1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab22_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc450cb113fd63a002f4e00f43c0fb80e280194")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99117631da72d22008fa3764f76c99c2137403d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab22_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab22_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
